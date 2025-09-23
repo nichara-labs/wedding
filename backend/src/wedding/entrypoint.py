366014cf5126defe2eb425184148a637af5bfc39
extrange <29305375+extrange@users.noreply.github.com>
2025-09-23 18:02:00 +0800

    fix: update CORS middleware to restrict allowed origins

--- backend/src/wedding/entrypoint.py
+++ backend/src/wedding/entrypoint.py
@@ -8,7 +8,7 @@ from wedding.routes import rsvp
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://chanelandnicholas.com", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
